9dbb3dde0dbc573de39c2fdcced482533799aa11
HARVAR <42223468+SUSANO-O@users.noreply.github.com>
2024-08-30 21:39:48 -0500

    add components

---
+++ src/components/view4.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function View4() {
+    return (
+        <div>
+            <h2>View 1</h2>
+            <p>Este es el contenido de la Vista 1.</p>
+        </div>
+    );
+}
+
+export default View4;
